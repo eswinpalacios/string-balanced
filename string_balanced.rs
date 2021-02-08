41f3f19c57b1ebda81976a6b3725da033eeed5b1
Eswin Palacios Sarmiento <eswinxd@gmail.com>
2021-02-07 22:51:51 -0500

    add solution in rust

---
+++ string_balanced.rs
@@ -0,0 +1,36 @@
+use std::collections::HashMap;
+
+fn is_string_balanced(list_characters: &str) -> bool {    
+    let open: HashMap<&char, char> = [ (&'{', '{'), (&'[', '('), (&'(', '(') ].iter().cloned().collect();
+    let close: HashMap<&char, char> = [ (&'}', '{'), (&']', '['), (&')', '(') ].iter().cloned().collect();
+    let mut stack:Vec<char> = Vec::new();    
+
+    for character in list_characters.chars() {
+        if open.contains_key(&character) {
+            stack.push(character)
+        } else if close.contains_key(&character) {
+            let c = close.get(&character);
+            if stack.len() == 0 {
+                return false
+            } else if Some(&stack[stack.len() - 1 ]) != c{
+                return false
+            } else {
+                stack.pop();
+            }
+        }
+    }
+
+    return if stack.len() == 0 { true } else { false }
+}
+
+fn main() {
+    println!("{}", is_string_balanced("(a[0]+b[2c[6]]) {24+53})"));
+    println!("{}", is_string_balanced("f(e(d))[()]{}([])'"));
+    println!("{}", is_string_balanced("((b)"));
+    println!("{}", is_string_balanced("(c]"));
+    println!("{}", is_string_balanced("{(a[])"));
+    println!("{}", is_string_balanced("([)]"));
+    println!("{}", is_string_balanced(")("));
+    println!("{}", is_string_balanced(""));
+    println!("{}", is_string_balanced("([)]"));
+}
